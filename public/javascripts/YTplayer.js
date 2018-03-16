e6cb0295d1baf0a2f07cd15e483fbdd38976cfaa
resghst <hank181017@gmail.com>
2018-03-17 06:47:54 +0800

    the video size can change by window

---
+++ public/javascripts/YTplayer.js
@@ -0,0 +1,51 @@
+$( document ).ready(function() {
+	$(window).on('resize',listensize);
+})
+
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+var player;
+var width = $( '.video' ).width();
+var height = width*390/640;
+console.log('height--'+height)
+console.log('width--'+width)
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+    height: height,
+    width: width,
+    videoId: 'NIqDt-2oQuw',
+    events: {
+    'onReady': onPlayerReady,
+    'onStateChange': onPlayerStateChange
+    }
+    });
+}
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+    event.target.playVideo();
+}
+// 5. The API calls this function when the player's state changes.
+//    The function indicates that when playing a video (state=1),
+//    the player should play for six seconds and then stop.
+var done = false;
+function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING && !done) {
+				// listensize()
+        setTimeout(stopVideo, 3000);
+        done = true;
+    }
+}
+function stopVideo() {
+    player.stopVideo();
+}
+function listensize(){
+	width = $( '.video' ).width();
+	height = width*390/640;
+	player.setSize(width,height)
+	console.log(width+ "--" +height)
+}
