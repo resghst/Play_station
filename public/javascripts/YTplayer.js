e0972e0876ddd678629ead40af63f29bd8733326
resghst <hank181017@gmail.com>
2018-03-23 15:18:54 +0800

    The player can play video by sequence

--- public/javascripts/YTplayer.js
+++ public/javascripts/YTplayer.js
@@ -12,40 +12,44 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 var player;
 var width = $( '.video' ).width();
 var height = width*390/640;
-console.log('height--'+height)
-console.log('width--'+width)
+// console.log('height--'+height)
+// console.log('width--'+width)
 function onYouTubeIframeAPIReady() {
-    player = new YT.Player('player', {
-    height: height,
-    width: width,
-    videoId: 'NIqDt-2oQuw',
-    events: {
-    'onReady': onPlayerReady,
-    'onStateChange': onPlayerStateChange
-    }
-    });
+	player = new YT.Player('player', {
+	height: height,
+	width: width,
+	videoId: 'JTEB0xKeaLY',
+	events: {
+		'onReady': onPlayerReady,
+		'onStateChange': onPlayerStateChange
+	}
+	});
 }
 // 4. The API will call this function when the video player is ready.
 function onPlayerReady(event) {
-    event.target.playVideo();
+	event.target.playVideo();
 }
 // 5. The API calls this function when the player's state changes.
 //    The function indicates that when playing a video (state=1),
 //    the player should play for six seconds and then stop.
 var done = false;
 function onPlayerStateChange(event) {
-    if (event.data == YT.PlayerState.PLAYING && !done) {
-				// listensize()
-        setTimeout(stopVideo, 3000);
-        done = true;
-    }
+	if (event.data == YT.PlayerState.PLAYING && !done) {
+		// setTimeout(stopVideo, 0);
+		// done = true;
+	}
+	if( player.getPlayerState() == 0 ){
+		player.loadVideoById("C0EviZmG5qM")
+		// console.log("done")
+	}
 }
 function stopVideo() {
-    player.stopVideo();
+	player.stopVideo();
+	// player.loadVideoById("m1CEs4gsQIQ", "large")
 }
 function listensize(){
 	width = $( '.video' ).width();
 	height = width*390/640;
-	player.setSize(width,height)
-	console.log(width+ "--" +height)
+	player.setSize(width,height);
+	// console.log(width+ "--" +height)
 }
