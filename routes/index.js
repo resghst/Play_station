6d801938cb6edaa8be0d2f16fff45b46c6d5de3c
resghst <hank181017@gmail.com>
2018-05-27 16:41:35 +0800

    single video time add in playlist is finish.

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,9 @@ var router = express.Router()
 var parse = require('url-parse')
 var request = require('request')
 
-var http = require("http").Server(express)
 var url = require('url');
 var fs = require('fs');
-var io = require('socket.io')(http) // 加入 Socket.IO
 let filepath  = './playlist/playlist.json'
-
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -20,35 +17,123 @@ router.get('/requestsong', function(req, res, next){
   querydata =  JSON.parse(req.query.query)
   url = parse(querydata.url, true)
   console.log(url.query.v)
+  playlist={}
   let vid = url.query.v
-  let qurl = 'https://www.googleapis.com/youtube/v3/videos?part=snippet&key=AIzaSyBtEa9HR713jZlCOh4xHD74bKxKqCavOhE&id='
-  qurl += vid
-  request({
-    url: qurl,
-    mathod: "GET"
-  }, 
-  (e, r, b)=>{
-    if( e || !b ){ return }
-    let resjson = JSON.parse(b)
-    let items = resjson.items
-    let videoitem={}
-    let playlist ={}
-    items.forEach(element => {
-      videoitem.title = element.snippet.title
-      videoitem.image = element.snippet.thumbnails.default.url
-      videoitem.vid = vid
-    });
-    fs.readFile( filepath , 'utf8', function(err, buffer){  
-      playlist = JSON.parse(buffer)
+
+  var delay = function(s){
+    return new Promise(function(resolve,reject){ setTimeout(resolve,s) })
+  }
+  delay()
+  .then(function(){
+    console.log('1')
+    fs.readFile( filepath , 'utf8', function(err, buffer){ playlist = JSON.parse(buffer)  }) 
+    return delay(2000)
+  })
+  .then(function(){
+    console.log('2')
+    console.log('oplaylist')
+    console.log(playlist)
+    let qurl = 'https://www.googleapis.com/youtube/v3/videos?part=snippet&key=AIzaSyBtEa9HR713jZlCOh4xHD74bKxKqCavOhE&id='
+    qurl += vid
+    request({
+      url: qurl,
+      mathod: "GET"
+    }, 
+    (e, r, b)=>{
+      if( e || !b ){ return }
+      let resjson = JSON.parse(b)
+      let items = resjson.items
+      let videoitem={}
+      items.forEach(element => {
+        videoitem.title = element.snippet.title
+        videoitem.image = element.snippet.thumbnails.default.url
+        videoitem.vid = vid
+      });
       playlist.title.push(videoitem.title)
       playlist.preimg.push(videoitem.image)
       playlist.videoid.push(videoitem.vid)
-      playlist.videotime = new Date()
-      playlist.novideo = false
-      fs.writeFile( filepath , JSON.stringify(playlist), 'utf8')
     }) 
-    res.json(videoitem)
+    return  delay(2000)
   })
+  .then(function(){
+    console.log('3')
+    let vtimequery = 'https://www.googleapis.com/youtube/v3/videos?part=contentDetails&key=AIzaSyBtEa9HR713jZlCOh4xHD74bKxKqCavOhE&id='
+    vtimequery += vid
+    request({
+      url: vtimequery,
+      mathod: "GET"
+    }, 
+    (e, r, b)=>{
+      if( e || !b ){ return }
+      console.log(playlist)
+      let resjson = JSON.parse(b)
+      let timestring = resjson.items[0].contentDetails.duration
+      playlist.videotime.push(timestring)
+    }) 
+    return  delay(2000)
+  })
+  .then(function(){
+    console.log('4')
+    playlist.ctime = new Date()
+    playlist.novideo = false
+    fs.writeFile( filepath , JSON.stringify(playlist), 'utf8')
+    console.log('fplaylist')
+    console.log(playlist)
+    res.json(playlist)
+  })
+  .catch((e)=>{
+    console.log(e)
+  })
+
+
+//=======================================================================================================================
+  function funct1(){
+    let vid = url.query.v
+    let qurl = 'https://www.googleapis.com/youtube/v3/videos?part=snippet&key=AIzaSyBtEa9HR713jZlCOh4xHD74bKxKqCavOhE&id='
+    qurl += vid
+    request({
+      url: qurl,
+      mathod: "GET"
+    }, 
+    (e, r, b)=>{
+      if( e || !b ){ return }
+      let resjson = JSON.parse(b)
+      let items = resjson.items
+      let videoitem={}
+      let playlist ={}
+      items.forEach(element => {
+        videoitem.title = element.snippet.title
+        videoitem.image = element.snippet.thumbnails.default.url
+        videoitem.vid = vid
+      });
+      playlist.title.push(videoitem.title)
+      playlist.preimg.push(videoitem.image)
+      playlist.videoid.push(videoitem.vid)
+    })
+  }
+
+//=======================================================================================================================
+  function funct2(){
+    let vtimequery = 'https://www.googleapis.com/youtube/v3/videos?part=contentDetails&key=AIzaSyBtEa9HR713jZlCOh4xHD74bKxKqCavOhE&id='
+    vtimequery += vid
+    request({
+      url: vtimequery,
+      mathod: "GET"
+    }, 
+    (e, r, b)=>{
+      if( e || !b ){ return }
+      let resjson = JSON.parse(b)
+      let timestring = resjson.items[0].contentDetails.duration
+      playlist.videotime.push(timestring)
+      console.log(timestring)
+    })
+  }
+
+
+  // playlist.videotime = new Date()
+  // playlist.novideo = false
+  // fs.writeFile( filepath , JSON.stringify(playlist), 'utf8')
+  // res.json(videoitem)
 
 })
 
